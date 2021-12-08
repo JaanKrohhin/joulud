18c1a87c22c758f153d083e7353e43ca6a2465b6
Opilane TTHK <opilane@tthk.ee>
2021-12-08 11:56:22 +0200

    Initial commit

---
+++ haid.js
@@ -0,0 +1,163 @@
+let x=20, y=20, r=4;
+let ysamm=3, xsamm=0, ykiirendus=0.4;
+let t, g; //tahvel, graafiline kontekst
+let colors=["red","white","yellow","darkviolet","pink","darkviolet"];
+
+function start(){
+    t=document.getElementById("tahvel");
+    g=t.getContext("2d");
+    tree();
+}
+function tree(){
+    g.clearRect(0, 0, t.width, t.height);
+    g.beginPath()
+    g.strokeStyle="brown";
+    g.fillStyle="brown";
+    g.fillRect(0,t.height-50,t.width,50);
+    g.stroke();
+    g.fill();
+    g.closePath();
+
+    g.beginPath()
+    g.strokeStyle="chocolate";
+    g.fillStyle="chocolate";
+    g.fillRect(t.width/3,t.height-50,t.width/3,-100);
+    g.stroke();
+    g.fill();
+    g.closePath();
+
+    g.beginPath()
+    g.strokeStyle="red";
+    g.fillStyle="red";
+    g.fillRect(t.width/3-20,t.height-50,t.width/3-40,-40);
+    g.strokeStyle="yellow";
+    g.fillStyle="yellow";
+    g.fillRect(t.width/3+20,t.height-50,t.width/3-120,-40);
+    g.fillRect(t.width/3-20,t.height-65,t.width/3-40,-10);
+    g.stroke();
+    g.fill();
+    g.closePath();
+
+
+
+    g.beginPath()
+    g.strokeStyle="green";
+    g.fillStyle="green";
+    g.moveTo(t.width/7,t.height-125)
+    g.lineTo(t.width-t.width/7,t.height-125)
+    g.lineTo(t.width/2,t.height-300)
+    g.stroke();
+    g.fill();
+    g.closePath();
+
+    g.beginPath()
+    g.moveTo(t.width/7,t.height-225)
+    g.lineTo(t.width-t.width/7,t.height-225)
+    g.lineTo(t.width/2,t.height-400)
+    g.stroke();
+    g.fill();
+    g.closePath();
+
+    g.beginPath()
+    g.moveTo(t.width/7,t.height-325)
+    g.lineTo(t.width-t.width/7,t.height-325)
+    g.lineTo(t.width/2,t.height-500)
+    g.stroke();
+    g.fill();
+    g.closePath();
+    garlands();
+}
+function garlands() {
+
+    g.beginPath();
+    g.strokeStyle=arrayRNG(colors);
+    g.lineWidth=rng(2,5);
+    g.moveTo(t.width/7+15,t.height-330)
+    g.bezierCurveTo(t.width/7+60,t.height-340,t.width/7+90,t.height-360,t.width/7+180,t.height-450);
+    g.stroke();
+    g.closePath();
+
+    g.beginPath()
+    g.strokeStyle=arrayRNG(colors);
+    g.lineWidth=rng(2,5);
+    g.moveTo(t.width/7+15,t.height-230)
+    g.bezierCurveTo(t.width/7+60,t.height-240,t.width/7+90,t.height-260,t.width/7+250,t.height-370);
+    g.stroke();
+    g.closePath();
+
+    g.beginPath();
+    g.strokeStyle=arrayRNG(colors);
+    g.lineWidth=rng(2,5);
+    g.moveTo(t.width/7+15,t.height-130);
+    g.bezierCurveTo(t.width/7+60,t.height-140,t.width/7+90,t.height-160,t.width/7+250,t.height-265);
+    g.stroke();
+    g.closePath();
+
+    g.beginPath();
+    g.strokeStyle=arrayRNG(colors);
+    g.lineWidth=rng(2,5);
+    g.moveTo((t.width-t.width/7)-15,t.height-130);
+    g.bezierCurveTo((t.width-t.width/7)-60,t.height-140,(t.width-t.width/7)-120,t.height-170,(t.width-t.width/7)-250,t.height-270);
+    g.stroke();
+    g.closePath();
+    toys();
+}
+function toys(){
+    g.beginPath()
+    g.strokeStyle=arrayRNG(colors);
+    for (let i = 0; i < 16; i++) {
+        g.fillStyle=arrayRNG(colors);
+        g.arc(t.width/7+35+i*12,t.height-330+i*10, r, 0, 2*Math.PI, true);
+    }
+    g.stroke();
+    g.fill();
+    g.closePath();
+
+    g.beginPath()
+    g.strokeStyle=arrayRNG(colors);
+    for (let i = 0; i < 14; i++) {
+        g.fillStyle=arrayRNG(colors);
+        g.arc(t.width/7+70+i*12,t.height-380+i*10, r, 0, 2*Math.PI, true);
+    }
+    g.stroke();
+    g.fill();
+    g.closePath();
+
+    g.beginPath()
+    g.strokeStyle=arrayRNG(colors);
+    for (let i = 0; i < 5; i++) {
+        g.fillStyle=arrayRNG(colors);
+        g.arc((t.width-t.width/7)-80-i*12,t.height-200+i*10, r, 0, 2*Math.PI, true);
+    }
+    g.stroke();
+    g.fill();
+    g.closePath();
+    star()
+}
+function star(){
+    g.beginPath()
+    g.strokeStyle="yellow";
+    g.fillStyle="yellow";
+    g.lineWidth=1
+    g.moveTo(t.width/2,t.height-560);
+    g.lineTo(t.width-(t.width/7)*3-15,t.height-530);
+    g.lineTo(t.width-(t.width/7)*3+20,t.height-528);
+    g.lineTo(t.width-(t.width/7)*3-12,t.height-510);
+    g.lineTo(t.width-(t.width/7)*3,t.height-480);
+    g.lineTo(t.width/2,t.height-500);
+    g.lineTo(t.width/7*3,t.height-480);
+    g.lineTo(t.width/7*3+12,t.height-510);
+    g.lineTo(t.width/7*3-20,t.height-528);
+    g.lineTo(t.width/7*3+15,t.height-530);
+    g.lineTo(t.width/2,t.height-560);
+    g.stroke();
+    g.fill();
+    g.closePath();
+}
+function rng(min,max){
+    return Math.random() * (max - min) + min;
+}
+function arrayRNG(array){
+    return array[Math.floor(Math.random() * array.length)]
+}
+
